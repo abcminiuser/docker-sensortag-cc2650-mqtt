bcbde2a93421c66d049b0875c725a6a15e3e077c
Dean Camera <dean@fourwalledcubicle.com>
2017-12-11 21:22:13 +1100

    Remove humidity sensing.

--- sensortag.py
+++ sensortag.py
@@ -15,9 +15,6 @@ class CC2530_SensorTag(object):
     CHAR_HANDLE_IR_TEMP_CONTROL = 0x0024
     CHAR_IR_TEMP_DATA = "F000AA01-0451-4000-B000-000000000000"
 
-    CHAR_HANDLE_HUMIDITY_CONTROL = 0x002C
-    CHAR_HUMIDITY_DATA = "F000AA21-0451-4000-B000-000000000000"
-
     def __init__(self, device):
         self._device = device
 
@@ -36,22 +33,6 @@ class CC2530_SensorTag(object):
 
         rawTamb = (value[3] << 8) + value[2]
         return float(rawTamb >> 2) * 0.03125
-
-    def enable_humidity_sensor(self):
-        self._device.char_write_handle(self.CHAR_HANDLE_HUMIDITY_CONTROL, [1])
-
-    def disable_humidity_sensor(self):
-        self._device.char_write_handle(self.CHAR_HANDLE_HUMIDITY_CONTROL, [0])
-
-    def read_humidity_sensor(self, retries=10):
-        value = [0]
-        for i in range(0, retries):
-            value = self._device.char_read(self.CHAR_HUMIDITY_DATA)
-            if all(v != 0 for v in value):
-                break
-
-        rawHumidity = (value[3] << 8) + value[2]
-        return float(rawHumidity & ~0x0003) / 65536 * 100
 
 
 while True:
@@ -75,22 +56,13 @@ while True:
                         "read": sensortag.read_ir_temp_sensor_ambient,
                         "format": "{:.2f}",
                     },
-                "humidity": {
-                        "topic": "home/sensortag/humidity",
-                        "sample_interval": 60 * 3,
-                        "sample_delay": 3,
-                        "enable": sensortag.enable_humidity_sensor,
-                        "disable": sensortag.disable_humidity_sensor,
-                        "read": sensortag.read_humidity_sensor,
-                        "format": "{:.2f}",
-                    },
             }
 
         while True:
             sensors_to_sample = dict((name, sensor) for name, sensor in sensor_list.items() if sensor.get("next_sample_time", 0) <= time.monotonic())
 
             for name, sensor in sensors_to_sample.items():
-                #print("Enabling \"{}\"...".format(name))
+                print("Enabling \"{}\"...".format(name))
                 sensor["enable"]()
 
             sample_delay = max([sensor.get("sample_delay", 0) for name, sensor in sensors_to_sample.items()])
@@ -103,7 +75,7 @@ while True:
                 formatter = sensor.get("format", "{}")
                 value_formatted = formatter.format(value)
 
-                #print("%s = %s" % (sensor["topic"], value_formatted))
+                print("%s = %s" % (sensor["topic"], value_formatted))
                 mqtt_client.publish(sensor["topic"], value_formatted);
 
                 #print("Disabling \"{}\"...".format(name))
